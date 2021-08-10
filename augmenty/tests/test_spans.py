6a29fc3b9543992380a10c189d835338e672ae3c
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2021-08-10 21:24:19 +0200

    expanded test

--- augmenty/tests/test_spans.py
+++ augmenty/tests/test_spans.py
@@ -14,43 +14,52 @@ def nlp():
 
 
 def test_create_ent_replace(nlp):
-    for nlp_ in [English(), nlp]:  # with and without a parser
-        doc = Doc(
-            nlp_.vocab,
-            words=[
-                "Augmenty",
-                "is",
-                "a",
-                "wonderful",
-                "tool",
-                "for",
-                "augmentation",
-                ".",
-            ],
-            spaces=[True] * 6 + [False] * 2,
-            ents=["B-ORG"] + ["O"] * 7,
-        )
+    nlp_ = English()
+    doc = Doc(
+        nlp_.vocab,
+        words=[
+            "Augmenty",
+            "is",
+            "a",
+            "wonderful",
+            "tool",
+            "for",
+            "augmentation",
+            ".",
+        ],
+        spaces=[True] * 6 + [False] * 2,
+        ents=["B-ORG"] + ["O"] * 7,
+    )
 
-        ent_augmenter = augmenty.load(
-            "ents_replace.v1", level=1.00, ent_dict={"ORG": [["SpaCy"]]}
-        )
+    ent_augmenter = augmenty.load(
+        "ents_replace.v1", level=1.00, ent_dict={"ORG": [["SpaCy"]]}
+    )
 
-        docs = list(augmenty.docs([doc], augmenter=ent_augmenter, nlp=nlp_))
+    docs = list(augmenty.docs([doc], augmenter=ent_augmenter, nlp=nlp_))
 
-        assert docs[0].text == "SpaCy is a wonderful tool for augmentation."
+    assert docs[0].text == "SpaCy is a wonderful tool for augmentation."
 
-        ent_augmenter = augmenty.load(
-            "ents_replace.v1", level=1.00, ent_dict={"ORG": [["The SpaCy Universe"]]}
-        )
+    ent_augmenter = augmenty.load(
+        "ents_replace.v1", level=1.00, ent_dict={"ORG": [["The SpaCy Universe"]]}
+    )
 
-        docs = list(augmenty.docs([doc], augmenter=ent_augmenter, nlp=nlp_))
+    docs = list(augmenty.docs([doc], augmenter=ent_augmenter, nlp=nlp_))
 
-        assert (
-            docs[0].text == "The SpaCy Universe is a wonderful tool for augmentation."
-        )
+    assert docs[0].text == "The SpaCy Universe is a wonderful tool for augmentation."
+
+    ent_augmenter = augmenty.load(
+        "ents_replace.v1",
+        level=1.00,
+        ent_dict={"PERSON": [["Kenneth"], ["Lasse", "Hansen"]]},
+    )
+
+    doc = nlp("My name is Jack.")
+    docs = list(augmenty.docs([doc], augmenter=ent_augmenter, nlp=nlp))
+
+    assert docs[0].text != "My name is Jack."
 
 
-def test_create_ent_replace(nlp):
+def test_create_per_replace(nlp):
     doc = Doc(
         nlp.vocab,
         words=["My", "name", "is", "Kenneth", "Enevoldsen"],
@@ -83,12 +92,26 @@ def test_create_ent_replace(nlp):
 
         assert docs[0].text == e
 
+    per_augmenter = augmenty.load(
+                "per_replace.v1",
+                level=1.00,
+                names={"firstname": ["Charles", "Jens"], "lastname": ["Kirkegaard", "Andersen"]},
+                patterns=[p],
+            )
+    text = "My name is Charlie."
+    doc = nlp(text)
+    docs = list(augmenty.docs([doc], augmenter=per_augmenter, nlp=nlp))
+    assert docs[0] != text
+
 
 def test_create_ent_format_augmenter(nlp):
     abbreviate = lambda token: token.text[0] + "."
 
     augmenter = augmenty.load(
-        "ents_format.v1", reordering=[-1, None], formatter=[None, abbreviate], level=1.00
+        "ents_format.v1",
+        reordering=[-1, None],
+        formatter=[None, abbreviate],
+        level=1.00,
     )
     texts = ["my name is Kenneth Enevoldsen"]
     aug_text = "my name is Enevoldsen K."
