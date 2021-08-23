06f4fe36cae1af91f1108337ad52851a8458b38b
Kenneth Enevoldsen <kennethcenevoldsen@gmail.com>
2021-08-23 11:25:41 +0200

    Update about.py

--- augmenty/about.py
+++ augmenty/about.py
@@ -1,3 +1,3 @@
 __title__ = "augmenty"
-__version__ = "0.0.7"  # the ONLY source of version ID
+__version__ = "0.0.8"  # the ONLY source of version ID
 __download_url__ = "https://github.com/kennethenevoldsen/augmenty"
