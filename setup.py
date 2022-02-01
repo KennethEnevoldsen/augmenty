9eeb02de80f3632963250a0262ce7cdec9282ba2
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-02-01 21:43:34 +0100

    Fixed setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-with open("spacy_wrap/about.py") as f:
+with open("augmenty/about.py") as f:
     v = f.read()
     for line in v.split("\n"):
         if line.startswith("__version__"):
@@ -9,8 +9,4 @@ with open("spacy_wrap/about.py") as f:
 
 
 def setup_package():
-    setup(name="spacy-wrap", version=__version__, packages=find_packages())
-
-
-if __name__ == "__main__":
-    setup_package()
+    setup(version=__version__, packages=find_packages())
