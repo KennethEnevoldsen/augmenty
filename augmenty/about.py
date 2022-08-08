bbd47ab0b6c90edb3544013a0444ffca1869065b
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-08-08 13:25:09 +0200

    build: Updated build dependencies

--- augmenty/about.py
+++ augmenty/about.py
@@ -1,4 +1,4 @@
 __title__ = "augmenty"
 
-__version__ = "1.0.2"  # the ONLY source of version ID
+__version__ = "1.0.3"  # the ONLY source of version ID
 __download_url__ = "https://github.com/kennethenevoldsen/augmenty"
